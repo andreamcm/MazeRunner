818e1ddbad783c4ca0a9ebbf2e5c5323d04c66bc
andreamcm <cor16076@uvg.edu.gt>
2017-05-12 19:56:58 -0600

    MazeRunner

---
+++ Maze Runner 2.0 copia/Assets/Scripts/Mover.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Mover : NetworkBehaviour {
+
+	private MazeCell currentCell;
+
+	private MazeDirection currentDirection;
+
+	private void Rotate (MazeDirection direction) {
+		transform.localRotation = direction.ToRotation();
+		currentDirection = direction;
+	}
+
+	public void SetLocation (MazeCell cell) {
+		currentCell = cell;
+		transform.localPosition = cell.transform.localPosition;
+	}
+
+	private void Move (MazeDirection direction) {
+		MazeCellEdge edge = currentCell.GetEdge(direction);
+		if (edge is MazePassage) {
+			SetLocation(edge.otherCell);
+		}
+	}
+
+	void Update()
+	{
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			Rotate(currentDirection.GetNextCounterclockwise());
+		}
+		else if (Input.GetKeyDown(KeyCode.E)) {
+			Rotate(currentDirection.GetNextClockwise());
+		}
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+	}
+
+
+}
